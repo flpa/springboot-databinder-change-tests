a2ca176fa7f90e1afa2649a70f4b5a15dad916b2
flpa <2512172+flpa@users.noreply.github.com>
2024-01-24 14:58:58 +0100

    add sample for Spring Boot 3.2.2

---
+++ databinder-test-boot3_2_2/src/main/java/com/example/databindertestboot3_2_2/DatabinderTestBoot322Application.java
@@ -0,0 +1,13 @@
+package com.example.databindertestboot3_2_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabinderTestBoot322Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabinderTestBoot322Application.class, args);
+	}
+
+}
---
+++ databinder-test-boot3_2_2/src/test/java/com/example/databindertestboot3_2_2/DatabinderTestBoot322ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.databindertestboot3_2_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabinderTestBoot322ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
